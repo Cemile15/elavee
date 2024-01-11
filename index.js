5e1b8cf69bd24e57b7693bfa5d41711ff56a2380
Cemile15 <qurbanlicemile344.gmail.com>
2024-01-11 15:09:47 +0400

    tavell

--- index.js
+++ index.js
@@ -1,41 +1,60 @@
-const travel1 =document.querySelector('.sec8');
-fetch("http://localhost:3000/travel")
-.then(res=>res.json())
-.then(data=>{
-    data.forEach(element => {
-        travel1.innerHTML+=`
-        <div class="carttt">
-        <div class="cart2">
-            <img class="cart2"
-                src="${element.image}"
-                alt="">
-        </div>
-        <div class="cart3">
-            <h1>${element.name}</h1>
-            <p class="text1">${element.category}</p>
-            <button onclick="deletebox(${element.id})" class="del">Delete</button>
-            <button onclick="Update(${element.id})" class="upd">Update</button>
-            <button onclick="favoritebox(${element.id})" class="fav">Favorite</button>
-            <a href="./details.html?id=${element.id}">details</a>
+const travel1 = document.querySelector('.sec8');
+let copy = [];
+function getAll() {
+    fetch("http://localhost:3000/travel")
+        .then(res => res.json())
+        .then(data => {
+            travel1.innerHTML = ""
+            copy = copy.length ?copy:data;
+            console.log(copy);
+            copy.forEach(element => {
+                travel1.innerHTML += `
+                <div class="carttt">
+                <div class="cart2">
+                    <img class="cart2"
+                        src="${element.image}"
+                        alt="">
+                </div>
+                <div class="cart3">
+                    <h1>${element.name}</h1>
+                    <p class="text1">${element.category}</p>
+                    <button onclick="deletebox(${element.id})" class="del">Delete</button>
+                    <button onclick="Update(${element.id})" class="upd">Update</button>
+                    <button onclick="favoritebox(${element.id})" class="fav">Favorite</button>
+                    <a href="./details.html?id=${element.id}">details</a>
 
-        </div>
-    </div>
+                </div>
+            </div>
         `
-    });
-})
+            });
+        });
+}
+getAll();
 
+sort.addEventListener("change", (e) => {
+    if (e.target.value == "asc") {
+        copy=copy.sort((a, b) => a.name.localeCompare(b.name));
+        console.log(copy);
+    }
+    else if (e.target.value == 'des') {
+        copy=copy.sort((a, b) => b.name.localeCompare(a.name));
+    } else {
+        copy = []
+    }
+    getAll();
+})
 
 
 //update
 
-function Update(id){
+function Update(id) {
     console.log(id);
-    axios.get(`http://localhost:3000/travel/${id}`).then((res)=>{
-        window.location=`./update.html?id=${res.data.id}`;
+    axios.get(`http://localhost:3000/travel/${id}`).then((res) => {
+        window.location = `./update.html?id=${res.data.id}`;
         console.log(res.data);
-        Name.value=res.data.name,
-        Cat.value=res.data.category,
-        imgDiv.src=res.data.image
+        Name.value = res.data.name,
+            Cat.value = res.data.category,
+            imgDiv.src = res.data.image
     })
 }
 
@@ -45,9 +64,9 @@ function Update(id){
 
 // delete
 
-function deletebox(id){
-    axios.delete("http://localhost:3000/travel/"+id)
-    window.location="./index.html"
+function deletebox(id) {
+    axios.delete("http://localhost:3000/travel/" + id)
+    window.location = "./index.html"
     console.log(element.id);
 
 }
@@ -55,11 +74,11 @@ function deletebox(id){
 
 // favorite
 
-function favoritebox(id){
-    axios.get(" http://localhost:3000/travel/"+id)
-    .then(res=>{
-        axios.post(" http://localhost:3000/Favorite", res.data)
-    }).then(res=>window.location="./favorite.html")
+function favoritebox(id) {
+    axios.get(" http://localhost:3000/travel/" + id)
+        .then(res => {
+            axios.post(" http://localhost:3000/Favorite", res.data)
+        }).then(res => window.location = "./favorite.html")
 }
 
 
